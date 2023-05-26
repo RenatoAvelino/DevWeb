40eddfa4782ce4115b0a7d27559b3754252933e8
Vinicius Cortêz <vinisicius2@gmail.com>
2023-05-26 18:24:20 -0300

    SettingsPage refatorada

--- FrontEnd/SettingsPage/script.js
+++ FrontEnd/SettingsPage/script.js
@@ -1,21 +1,15 @@
 function alterarImagens() {
-    const links = document.querySelectorAll('link[href*=".css"]');
-    const imgs = Array.from(document.getElementsByClassName("ButtonImages"));
+  const html = document.querySelector('html')
+  const imgs = Array.from(document.getElementsByClassName("ButtonImages"));
 
-    if (links[0].href.includes("StyleLight.css"))  {  
-      // Altera a imagem 
-      imgs.forEach((img) => {
+
+  if (html.classList.contains('dark-mode')) {
+    imgs.forEach((img) => {
         img.src= img.src.replace('-white.png', '.png');
       });
-    } else if (links[0].href.includes("StyleDark.css")){
-       // Altera a imagem 
+    } else {
       imgs.forEach((img) => {
-        img.src= img.src.replace('.png', '-white.png');
-      });
-    } else if (links[0].href.includes("StyleAcessibilidade.css") && !(imgs[0].src.includes("-white"))){
-        // Altera a imagem 
-       imgs.forEach((img) => {
-         img.src= img.src.replace('.png', '-white.png');
-       });
-     }  
-  };
+      img.src= img.src.replace('.png', '-white.png');
+    });
+    }
+};
