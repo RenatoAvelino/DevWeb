c58522e55dfc69cfe2e9feb3979d4015b0e8092d
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2023-05-28 17:26:57 -0300

    Correção Forms e TODO fontsize

---
+++ FrontEnd/MainPage/script.js
@@ -0,0 +1,29 @@
+var $affectedElements = $("p, h1, h2, h3, h4, h5, h6"); // Can be extended, ex. $("div, p, span.someClass")
+
+// Storing the original size in a data attribute so size can be reset
+$affectedElements.each( function(){
+  var $this = $(this);
+  $this.data("orig-size", $this.css("font-size") );
+});
+
+$("#btn-increase").click(function(){
+  changeFontSize(1);
+})
+
+$("#btn-decrease").click(function(){
+  changeFontSize(-1);
+})
+
+$("#btn-orig").click(function(){
+  $affectedElements.each( function(){
+        var $this = $(this);
+        $this.css( "font-size" , $this.data("orig-size") );
+   });
+})
+
+function changeFontSize(direction){
+    $affectedElements.each( function(){
+        var $this = $(this);
+        $this.css( "font-size" , parseInt($this.css("font-size"))+direction );
+    });
+}
