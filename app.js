026bf106586fc7b18821112fce579d80f7a1f4e4
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2023-06-04 18:59:25 -0300

    Novas paginas com template

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.render('pages/base/NaoFeitoAinda')
+    //res.render('pages/base/NaoFeitoAinda')
+    res.render('pages/login/LoginPage')
 })
 
 app.get('/main', (req, res) => {
-    res.render('pages/main/main')
+    res.render('pages/main/MainPage')
+})
+
+app.get('/forms', (req, res) => {
+    res.render('pages/forms/FormsPage')
+})
+
+app.get('/settings', (req, res) => {
+    res.render('pages/settings/SettingsPage')
+})
+
+app.get('/reports', (req, res) => {
+    res.render('pages/reports/ReportsPage')
+})
+
+app.get('/todo', (req, res) => {
+    res.render('pages/base/NaoFeitoAinda')
+})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login/LoginPage')
 })
 
 app.listen(port, () => {
