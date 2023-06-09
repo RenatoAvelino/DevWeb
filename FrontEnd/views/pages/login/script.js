a17234762a876b25154d57c03ddb6415f6eae6de
Vinicius Cortêz <vinisicius2@gmail.com>
2023-06-09 00:38:30 -0300

    [#8]: Transformando o login de requisição assincrona(com fetch) para sincrona(com xhttprequest) para assim garantir que nosso projeto tem ambos tipos de request

--- FrontEnd/views/pages/login/script.js
+++ FrontEnd/views/pages/login/script.js
@@ -1,54 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const clickLogButton = document.getElementById("ClickLog")
     const errorContainer = document.getElementById("errorContainer")
-  
+
     clickLogButton.addEventListener("click", () => {
         const username = document.getElementById("uname").value
         const password = document.getElementById("passw").value
 
         const endpoint = "http://localhost:8000/login"
 
-        // Crie um objeto para enviar os dados do formulário no corpo da solicitação
+        const xhr = new XMLHttpRequest()
+        xhr.open('POST', endpoint, false) // O terceiro parâmetro define a requisição síncrona
+        xhr.setRequestHeader('Content-Type', 'application/json')
+
         const formData = {
         login: username,
         password: password
-        };
+        }
 
-        fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(formData)
-        })
-        .then(res => res.json())
-        .then(res => {
-            if (!res.error) {
-                // Verifica se existe mensagem de erro
-                return res
-            } else {
-                //Dispara novo erro
-                throw new Error(res.error)
-            }
-        })
-        .then(data => {
-            // Salvar o token JWT no localStorage
+        xhr.send(JSON.stringify(formData))
+
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText)
+            //Salva o token JWT
             localStorage.setItem("token", data.token)
-
             // Navegar para a página "/main"
             window.location.href = "/main"
-        })
-        .catch(error => {
-            // Exibir mensagem de erro usando notificação push ou mensagem embutida
-            const errorContainer = document.getElementById("errorContainer")
-            // Opção 3: Notificação push
-            if (window.Notification && window.Notification.permission === "granted") {
-            new Notification({ body: error})
-            }
-            // Opção 4: Mensagem embutida
-            errorContainer.textContent = error
+        } else {
+            const data = JSON.parse(xhr.responseText)
+            errorContainer.textContent = 'Erro:' + data.error
             errorContainer.style.display = "block"
-        })      
+            throw new Error(data.error)
+        }
     })
 })
   
