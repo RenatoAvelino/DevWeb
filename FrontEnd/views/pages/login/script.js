141e3ef36c1910327281963b9cf30884fcbbc37f
Vinicius Cortêz <vinisicius2@gmail.com>
2023-06-09 00:07:41 -0300

    [#8]: Login integrado, mostrando mensagem de erro ao usuario quando login invalido e redirecionando para a /main quando o login dá certo

--- FrontEnd/views/pages/login/script.js
+++ FrontEnd/views/pages/login/script.js
@@ -3,42 +3,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const errorContainer = document.getElementById("errorContainer")
   
     clickLogButton.addEventListener("click", () => {
-      const username = document.getElementById("uname").value;
-      const password = document.getElementById("passw").value;
+        const username = document.getElementById("uname").value
+        const password = document.getElementById("passw").value
 
-      const endpoint = "http://localhost:8000/login"
-  
-      // Crie um objeto para enviar os dados do formulário no corpo da solicitação
-      const formData = {
+        const endpoint = "http://localhost:8000/login"
+
+        // Crie um objeto para enviar os dados do formulário no corpo da solicitação
+        const formData = {
         login: username,
         password: password
-      };
-  
-      fetch(endpoint, {
+        };
+
+        fetch(endpoint, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(formData)
-      })
+        })
         .then(res => res.json())
+        .then(res => {
+            if (!res.error) {
+                // Verifica se existe mensagem de erro
+                return res
+            } else {
+                //Dispara novo erro
+                throw new Error(res.error)
+            }
+        })
         .then(data => {
-          // Manipule a resposta do servidor após o login
-          console.log(data);
+            // Salvar o token JWT no localStorage
+            localStorage.setItem("token", data.token)
+
+            // Navegar para a página "/main"
+            window.location.href = "/main"
         })
         .catch(error => {
             // Exibir mensagem de erro usando notificação push ou mensagem embutida
             const errorContainer = document.getElementById("errorContainer")
-            const errorMessage = '<%= errorMessage %>'
-            //const errorMessage = `Erro no login: ${error.message}`;
             // Opção 3: Notificação push
             if (window.Notification && window.Notification.permission === "granted") {
-              new Notification("Erro", { body: errorMessage });
+            new Notification({ body: error})
             }
             // Opção 4: Mensagem embutida
-            errorContainer.textContent = errorMessage;
-            errorContainer.style.display = "block";
-        })
-    });
-  });
+            errorContainer.textContent = error
+            errorContainer.style.display = "block"
+        })      
+    })
+})
   
