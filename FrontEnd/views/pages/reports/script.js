afc1a8a97872c8d4ed23c3daa36e140257773c71
Vinicius Cortêz <vinisicius2@gmail.com>
2023-06-24 11:13:32 -0300

    Grafico com as duas ultimas datas

--- FrontEnd/views/pages/reports/script.js
+++ FrontEnd/views/pages/reports/script.js
@@ -1,7 +1,3 @@
-const moment = window.moment
-
-Chart.register(ChartDataLabels)
-
 function alterarImagens() {
   const html = document.querySelector('html')
   const imgs = Array.from(document.getElementsByClassName('AmbSoc'))
@@ -20,69 +16,88 @@ function alterarImagens() {
   })
 }
 
-function createChart() {
-  // Carregue os dados do arquivo CSV usando a biblioteca d3.js
-  d3.csv('/data').then(function (data) {
-    // Formate as datas no formato correto
-    var parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S')
-    data.forEach(function (d) {
-      d.DATE_TIME = parseDate(d.DATE_TIME)
-      d.AC_POWER = +d.AC_POWER
-    })
+window.addEventListener('DOMContentLoaded', function () {
+  const moment = window.moment
 
-    // Agrupe os dados pela data e calcule a soma da AC_POWER
-    var groupedData = d3.group(data, function (d) {
-      return d.DATE_TIME
-    })
-    var aggregatedData = Array.from(groupedData, function ([key, values]) {
-      return { DATE_TIME: key, AC_POWER: d3.sum(values, function (d) { return d.AC_POWER }) }
-    })
+  Chart.register(ChartDataLabels)
 
-    // Extrai as datas e os valores da AC_POWER em arrays separados
-    var labels = aggregatedData.map(function (d) { return d.DATE_TIME })
-    var values = aggregatedData.map(function (d) { return d.AC_POWER })
+  function createChart() {
+    var fromDate = moment().subtract(1, 'day').startOf('day').format('YYYY-MM-DD HH:mm')
+    var toDate = moment().endOf('day').format('YYYY-MM-DD HH:mm')
 
-    // Cria um elemento canvas para renderizar o gráfico
-    var canvas = document.createElement('canvas')
-    document.getElementById('Graf').appendChild(canvas)
+    // Verificar se as datas foram preenchidas no formulário
+    var deInput = document.getElementById('de')
+    var ateInput = document.getElementById('ate')
 
-    // Cria o gráfico usando Chart.js
-    var ctx = canvas.getContext('2d')
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'AC_POWER',
-          data: values,
-          fill: false,
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'day',
-              tooltipFormat: 'YYYY-MM-DD HH:mm:ss',
-              displayFormats: {
-                day: 'YYYY-MM-DD HH:mm:ss'
+    if (deInput.value && ateInput.value) {
+      fromDate = moment(deInput.value, 'DD/MM/YYYY').startOf('day').format('YYYY-MM-DD HH:mm')
+      toDate = moment(ateInput.value, 'DD/MM/YYYY').endOf('day').format('YYYY-MM-DD HH:mm')
+    }
+
+    // Carregue os dados do arquivo CSV usando a biblioteca d3.js
+    d3.csv('/data').then(function (data) {
+      // Formate as datas no formato correto
+      var parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S')
+      data.forEach(function (d) {
+        d.DATE_TIME = parseDate(d.DATE_TIME)
+        d.AC_POWER = +d.AC_POWER
+      })
+
+      // Filtrar os dados pelo período selecionado
+      data = data.filter(function (d) {
+        return moment(d.DATE_TIME).isBetween(fromDate, toDate)
+      })
+
+      // Agrupe os dados pela data e calcule a soma da AC_POWER
+      var groupedData = d3.group(data, function (d) {
+        return d.DATE_TIME
+      })
+      var aggregatedData = Array.from(groupedData, function ([key, values]) {
+        return { DATE_TIME: key, AC_POWER: d3.sum(values, function (d) { return d.AC_POWER }) }
+      })
+
+      // Extrai as datas e os valores da AC_POWER em arrays separados
+      var labels = aggregatedData.map(function (d) { return d.DATE_TIME })
+      var values = aggregatedData.map(function (d) { return d.AC_POWER })
+
+      // Cria um elemento canvas para renderizar o gráfico
+      var canvas = document.createElement('canvas')
+      document.getElementById('Graf').appendChild(canvas)
+
+      // Cria o gráfico usando Chart.js
+      var ctx = canvas.getContext('2d')
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels.map(function (d) { return moment(d).format('HH:mm:ss') }),
+          datasets: [{
+            label: 'AC_POWER',
+            data: values,
+            fill: false,
+            borderColor: 'rgba(233, 147, 71, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'YYYY-MM-DD HH:mm',
+                displayFormats: {
+                  day: 'YYYY-MM-DD HH:mm'
+                }
               }
+            },
+            y: {
+              beginAtZero: true
             }
-          },
-          y: {
-            beginAtZero: true
           }
         }
-      }
+      })
     })
-  })
-}
+  }
 
-// Chame as funções createChart() e alterarImagens() quando a página for carregada
-window.addEventListener('DOMContentLoaded', function () {
-  alterarImagens()
   createChart()
 })
