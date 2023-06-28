83ee17ecfa01bacb1eefbb985c14c315206e5600
Vinicius Cortêz <vinisicius2@gmail.com>
2023-06-28 18:11:32 -0300

    Grafico finalmente construido

--- FrontEnd/views/pages/reports/script.js
+++ FrontEnd/views/pages/reports/script.js
@@ -21,82 +21,134 @@ window.addEventListener('DOMContentLoaded', function () {
 
   Chart.register(ChartDataLabels)
 
-  function createChart() {
-    var fromDate = moment().subtract(1, 'day').startOf('day').format('YYYY-MM-DD HH:mm')
-    var toDate = moment().endOf('day').format('YYYY-MM-DD HH:mm')
-  
-    // Verificar se as datas foram preenchidas no formulário
-    var deInput = document.getElementById('de')
-    var ateInput = document.getElementById('ate')
-  
-    if (deInput.value && ateInput.value) {
-      fromDate = moment(deInput.value, 'DD/MM/YYYY').startOf('day').format('YYYY-MM-DD HH:mm')
-      toDate = moment(ateInput.value, 'DD/MM/YYYY').endOf('day').format('YYYY-MM-DD HH:mm')
-    }
-  
+  function createChart() { 
     // Carregue os dados do arquivo CSV usando a biblioteca d3.js
     d3.csv('/data').then(function (data) {
-      // Formate as datas no formato correto
-      var parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S')
       var formattedDates = []
       var acPowerValues = []
 
-      data.forEach(function (d) {
-        d.DATE_TIME = parseDate(d.DATE_TIME)
-        d.AC_POWER = +d.AC_POWER
 
-        formattedDates.push(d.DATE_TIME)
+      // Verificar se as datas foram preenchidas no formulário
+      var deInput = document.getElementById('de')
+      var ateInput = document.getElementById('ate')
+    
+      if (deInput.value && ateInput.value) {
+        var fromDate = moment(deInput.value, 'DD/MM/YYYY').startOf('day').format('YYYY-MM-DD HH:mm')
+        var toDate = moment(ateInput.value, 'DD/MM/YYYY').endOf('day').format('YYYY-MM-DD HH:mm')
+      } else{
+        var lastDate = data[data.length - 1]
+        var toDate = moment(lastDate.DATE_TIME, 'YYYY-MM-DD HH:mm')
+        var fromDate = toDate.clone().subtract(0, 'day').startOf('day')
+      }
+
+      fromDate = new Date(fromDate._d)
+      toDate = new Date(toDate._d)
+
+      // Filtrar os dados pelo período selecionado
+      data = data.filter(function (d) {
+        return moment(d.DATE_TIME).isBetween(fromDate, toDate)
+      })
+
+      // Agrupe os dados pela data e calcule a soma da AC_POWER
+      var groupedData = d3.group(data, function (d) {
+        var formattedDate = moment(d.DATE_TIME).format('YYYY-MM-DD HH:mm')
+        formattedDates.push(formattedDate)
         acPowerValues.push(d.AC_POWER)
       })
-      console.log(formattedDates)
-      console.log(acPowerValues)
+
+     // Agrupe os dados pelos valores distintos da data e calcule a soma da AC_POWER
+    var groupedData = d3.rollup(data, function (values) {
+      return d3.sum(values, function (d) { return d.AC_POWER })
+    }, function (d) {
+      return moment(d.DATE_TIME).format('YYYY-MM-DD HH:mm')
+    })
+
+    var aggregatedData = Array.from(groupedData, function ([key, value]) {
+      return { DATE_TIME: key, AC_POWER: value }
+    })
+
+    // Extrai as datas e os valores da AC_POWER em arrays separados
+    var labels = aggregatedData.map(function (d) { return d.DATE_TIME })
+    var values = aggregatedData.map(function (d) { return d.AC_POWER })
+
+    console.log(labels)
+    console.log(values)
   
-      // Extrai as datas e os valores da AC_POWER em arrays separados
-      var labels = data.map(function (d) { return moment(d.DATE_TIME).format('HH:mm:ss') })
-      var values = data.map(function (d) { return d.AC_POWER })
-      console.log(labels)
-      console.log(values)
-  
-      // Cria um elemento canvas para renderizar o gráfico
-      var canvas = document.createElement('canvas')
-      document.getElementById('Graf').appendChild(canvas)
-  
-      // Cria o gráfico usando Chart.js
-      var ctx = canvas.getContext('2d')
-      new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: 'AC_POWER',
-            data: values,
-            fill: false,
-            borderColor: 'rgba(233, 147, 71, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            x: {
-              type: 'time',
-              time: {
-                unit: 'day',
-                tooltipFormat: 'YYYY-MM-DD HH:mm',
-                displayFormats: {
-                  day: 'YYYY-MM-DD HH:mm'
-                }
+    // Cria um elemento canvas para renderizar o gráfico
+    var canvas = document.createElement('canvas')
+    document.getElementById('Graf').appendChild(canvas)
+
+    // Cria o gráfico usando Chart.js
+    var ctx = canvas.getContext('2d')
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'AC_POWER',
+          data: values,
+          fill: true, // Preenche o lado interno da curva com a cor da linha
+          borderColor: 'rgba(233, 147, 71, 1)',
+          backgroundColor: 'rgba(233, 147, 71, 0.4)', // Define a cor de preenchimento do lado interno da curva
+          borderWidth: 1,
+          pointRadius: 2 // Oculta os pontos na linha principal do gráfico
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            display: true,
+            type: 'time',
+            time: {
+              unit: 'day',
+              tooltipFormat: 'DD-MM-YYYY HH:mm',
+              displayFormats: {
+                day: 'DD-MM-YYYY HH:mm'
               }
             },
-            y: {
-              beginAtZero: true,
-              title: {
-                display: true,
-                text: 'AC Power'
+            title: {
+              display: true,
+              text: 'Período medido'
+            }
+          },
+          y: {
+            display: true,
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'AC Power(W)'
+            },
+            ticks: {
+              callback: function(value) {
+                return value.toLocaleString('pt-BR', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2
+                })
               }
             }
           }
+        },
+        plugins: {
+          tooltip: {
+            mode: 'nearest', // Exibe o tooltip mais próximo ao posicionar o mouse sobre o gráfico
+            intersect: false, // Permite que o tooltip seja exibido mesmo fora dos pontos da linha
+            callbacks: {
+              label: function(context) {
+                var value = context.parsed.y.toLocaleString('pt-BR', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2
+                })
+                return value;
+              }
+            }
+          },
+          datalabels: {
+            display: false // Oculta os valores da linha
+          }
         }
-      })
+      }
+    })
+
     })
   }
   
